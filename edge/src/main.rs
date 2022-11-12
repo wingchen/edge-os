b74a039a6338e435a7224a80e0d324e6e95a3bb3
Winston Tan <winston@aioniclabs.com>
2022-11-11 23:20:23 -0800

    download websocat for ssh <> websocket bridge

--- edge/src/main.rs
+++ edge/src/main.rs
@@ -15,8 +15,11 @@ async fn main() {
         Err(_e) => "/opt/edge-os-edge".to_string(),
     };
 
-    let device_id = config::get_device_id(local_working_dir);
+    let device_id = config::get_device_id(local_working_dir.clone());
     info!("Starting edge-os-edge: {device_id}");
+
+    config::get_websocat(local_working_dir.clone()).await;
+    info!("websocat is properly installed");
 
     let cloud_server_url = match env::var("EDGE_OS_CLOUD_URL") {
         Ok(val) => val,
